ef8d73c3bdbce5f9d4c610b68963701bfdecbad8
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-10-22 09:49:34 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.fdf1ff102f5ece29e9ce5f3eef797527.js"
 );
 
 self.addEventListener('message', (event) => {
